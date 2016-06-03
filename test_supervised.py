0ac5794d71f2b6ed82de9c5711ca48c9063e37fa
s4sarath <s4sarath@gmail.com>
2016-06-03 11:13:32 +0530

    added word2vec supervised file inside gensim zip

--- test_supervised.py
+++
@@ -1,7 +0,0 @@
-
-try:
-	from gensim.models.word2vec_inner_supervised import train_sentence_sg_mod, train_sentence_sg, train_sentence_sg_categ, train_sentence_sg_categ_nogil, train_sentence_cbow, FAST_VERSION
-	print 'Success'
-except:
-	print 'Failed to load train_sentence_sg_mod from gensim.models.word2vec_inner_supervised '
-
---
+++ test_word2vec_supervised.py
@@ -0,0 +1,28 @@
+
+from gensim.models.word2vec_supervised import word2vec_supervised
+
+
+########## A Small Demo 
+### Better to provide min_count = 1 ( otheriwse make sure your labels
+## occur more than min_count .) .
+
+### Labels must provide in a dictionary format . key : [list of l
+### Multiple labels are also possible . Even single label , it should be in 
+### a list
+
+
+
+sentences = [ ["skirt"  , "shirt" , "pants"] ,
+			  ["lipstick" , "top" , "scarf"] ]
+
+label_dict = { "skirt": ["women_category"] ,
+			   "shirt": ["men_category"] , 
+			   "lipstick":["women_category"],
+			   "top":["women_category" , "men_category"] ,
+			   "scarf":["women_category"]}
+
+model = Word2Vec_Supervised( sentences , min_count=1, window=5, hs=0 , negative = 7 , label_dict = label_dict , workers = 4)
+
+##### Once the model is trained , you can find the top categories by
+
+model.most_similar_category(["skirt"])
